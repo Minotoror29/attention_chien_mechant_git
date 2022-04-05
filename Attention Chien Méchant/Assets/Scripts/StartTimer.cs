4610f72858f60ea2e7fa930d44a659cb4bc9a60f
Minotoror29 <matthieu.boissel@laposte.net>
2022-04-05 14:39:29 +0200

    timer

---
+++ Attention Chien Méchant/Assets/Scripts/StartTimer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartTimer : MonoBehaviour
+{
+    private float timer = 3;
+    private int timeOnText;
+    [SerializeField] private Text timerText;
+    [SerializeField] private GameObject timerCanvas;
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        } else
+        {
+            timerCanvas.SetActive(false);
+        }
+
+        if (timer > 2)
+        {
+            timeOnText = 3;
+        } else if (timer > 1)
+        {
+            timeOnText = 2;
+        } else if (timer > 0)
+        {
+            timeOnText = 1;
+        }
+
+        timerText.text = timeOnText.ToString();
+    }
+}
